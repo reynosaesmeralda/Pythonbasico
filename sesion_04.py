0f59bf10fbe92a77d9df9747c0a45aecafa007fb
blancayessenia <blancayesseniae@gmail.com>
2026-02-17 20:43:44 -0800

    actualizar descripcion

--- sesion_04.py
+++ sesion_04.py
@@ -1,26 +1,10 @@
-# Instrucciones Función Calculadora:
-# Cree un función en python llamada calculadora, la cual debe tomar los parámetros (num_1, num_2, operacion) y debe ser capaz de realizar toda la lógica de un calculadora simple como: sumar, restar, multiplicar y dividir.
-# Nota: Las entradas serán proporcionadas por el usuario.
-# Pista: Este código es un ejemplo de una suma 
-
-#num_1 = float(input("Escriba el valor del primer numero: "))
-#num_2 = float(input("Escriba el valor del segundo numero: "))
-#operacion = input("¿Cual operacion deseas hacer? +, -, *, / -> ")
-
-#ef suma(num_1, num_2):
-#    return num_1 + num_2
-
-#if operacion == "+":
-#    resultado = suma(num_1, num_2)
-#    print("El resultado de la suma es: ", resultado)
 
 print("=== Calculadora ===")
-
-num_1 = float(input("Primer numero: "))
-num_2 = float(input("Segundo numero: "))
-operacion = input("Operacion (+, -, *, /): ")
-
-def calculadora(num_1, num_2, operacion):
+n1 = float(input("numero: "))
+n2 = float(input("numero: "))
+o = input("Operacion +-*/: ")
+print("Resultado:", c(n1, n2, o))
+def c(num_1, num_2, operacion):
     if operacion == "+":
         return num_1 + num_2
     if operacion == "-":
@@ -32,5 +16,3 @@ def calculadora(num_1, num_2, operacion):
             return "No se puede dividir entre 0"
         return num_1 / num_2
     return "Operacion no valida"
-
-print("Resultado:", calculadora(num_1, num_2, operacion))
