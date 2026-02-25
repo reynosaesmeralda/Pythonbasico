c2ccb1f40520df37bcca74372ee3741691166156
reynosaesmeralda25 <reynosaesmeralda25@gmail.com>
2026-02-24 16:20:58 -0800

    clase enemigo

---
+++ Enemigo.py
@@ -0,0 +1,21 @@
+class Enemigo:
+    tipo_enemigo: str
+    punto_energia: int = 10
+    ataque = 1
+
+def __init__(self, tipo_enemigo, punto_energia = 10 , ataque = 1):
+    self.__tipo_enemigo = tipo_enemigo
+    self.puntos_energia = punto_energia
+    self.ataque = ataque
+
+def get_tipo_enemigo(self):
+    return self.__tipo_enemigo
+
+def habla(self):
+    print(f"Yo sor {self.__tipo_enemigo}. Preparado para pelear!!")
+
+def camina(self):
+    print(f"{self.__tipo_enemigo} se mueve cerca de ti!!!")
+
+def atacar(self):
+    print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño!!")        
